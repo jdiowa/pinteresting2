de720967742ccad66a082c26a8f91b1b858b2837
Jeremy DeWitt <jeremy@executiveimpactonline.com>
2014-12-18 00:04:22 -0600

    Implemented Devise for Users

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Pinteresting2::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  # Required for Heroku
+    config.action_mailer.default_url_options = { host: 'http://jd-pinteresting2.herokuapp.com/' }
 end
